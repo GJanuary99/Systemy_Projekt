34aaa95c5012403e23ee138b8568eed85968972e
root <vixier61@gmail.com>
2021-06-10 19:08:46 +0200

    Dodano wielowątkowość i zwiększono ilość samochodów + dodana kolizja

---
+++ Projekt_Systemy/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Projekt_Systemy
+{
+    public class Car
+    {
+        const int maxCarSpeed = 2;
+        const int minCarSpeed = 10;
+        public int speed;
+        public Point coordinates = new Point(-50, 110);
+        public Point bezieraHorizontal;
+        public Point bezieraVertical;
+        public int skret = 1;
+        public int procentTurn = 0;
+
+        public Car()
+        {
+        }
+
+        public void getNewValues(Random rand)
+        {
+            coordinates = new Point(-50, 110);
+            skret = 1;
+            procentTurn = 0;
+            speed = rand.Next(maxCarSpeed, minCarSpeed);
+        }
+    }
+}
--- Projekt_Systemy/MainWindow.xaml.cs
+++ Projekt_Systemy/MainWindow.xaml.cs
@@ -30,103 +30,78 @@ namespace Projekt_Systemy
         Random rand = new Random();
         const int minTrainTime = 5000;
         const int maxTrainTime = 10000;
-        bool lightSignal = false;
+        public bool lightSignal = false;
 
         private Point betweenPoint(Point one, Point two, int procent, int maxProcent)
         {
             return new Point(one.X+(((two.X-one.X)*procent)/ maxProcent), one.Y + (((two.Y - one.Y) * procent)/ maxProcent));
         }
 
-        private void StartCar()
+        private void StartCar(Car car, Image image)
         {
-            Point xy = new Point(-50, 110);
-            Point bezieraHorizontal = new Point(510, 110);
-            Point bezieraVertical = new Point(610, 110);
-            int procentTurn = 0;
-
-
-            while (xy.X > 510)
+            while (true)
             {
-                this.Dispatcher.Invoke((Action)(() =>
+                Thread.Sleep(rand.Next(minTrainTime / 4, maxTrainTime / 4));
+                while (car.coordinates.X < 520)
                 {
-                    MoveCar(xy, car1);
-                }));
-                System.Threading.Thread.Sleep(5);
-                xy.X++;
-            }
-            while (bezieraVertical.Y > 200)
-            {
-                xy = betweenPoint(bezieraHorizontal, bezieraVertical, procentTurn, 90);
-                this.Dispatcher.Invoke((Action)(() =>
-                {
-                    MoveCar(xy, car1);
-                }));
-                bezieraHorizontal.X++;
-                bezieraVertical.Y++;
-                procentTurn++;
-                System.Threading.Thread.Sleep(10);
-            }
-            bezieraHorizontal.Y = 290;
-            while (bezieraVertical.Y > 290)
-            {
-                xy = betweenPoint(bezieraHorizontal, bezieraVertical, procentTurn, 90);
-                this.Dispatcher.Invoke((Action)(() =>
-                {
-                    MoveCar(xy, car1);
-                }));
-                bezieraHorizontal.X--;
-                bezieraVertical.Y++;
-                procentTurn--;
-                System.Threading.Thread.Sleep(10);
-            }
-            while (xy.X < 170)
-            {
-                this.Dispatcher.Invoke((Action)(() =>
-                {
-                    MoveCar(xy, car1);
-                }));
-                System.Threading.Thread.Sleep(5);
-                xy.X--;
-            }
-            bezieraHorizontal.X = 170;
-            bezieraVertical.X = 100;
-            while (bezieraVertical.Y > 350)
-            {
-                xy = betweenPoint(bezieraHorizontal, bezieraVertical, procentTurn, 60);
-                this.Dispatcher.Invoke((Action)(() =>
-                {
-                    MoveCar(xy, car1);
-                }));
-                bezieraHorizontal.X--;
-                bezieraVertical.Y++;
-                procentTurn++;
-                System.Threading.Thread.Sleep(10);
-            }
-            bezieraHorizontal.Y = 410;
-            while (bezieraVertical.Y > 410)
-            {
-                xy = betweenPoint(bezieraHorizontal, bezieraVertical, procentTurn, 60);
-                this.Dispatcher.Invoke((Action)(() =>
-                {
-                    MoveCar(xy, car1);
-                }));
-                bezieraHorizontal.X++;
-                bezieraVertical.Y++;
-                procentTurn--;
-                System.Threading.Thread.Sleep(10);
-            }
-            while (xy.X > 800)
-            {
-                while (lightSignal && xy.X>590 && xy.X<610)
-                {
-                    System.Threading.Thread.Sleep(50);
+                    MoveCar(car.coordinates, image, car.skret);
+                    System.Threading.Thread.Sleep(car.speed);
+                    car.coordinates.X++;
                 }
-                this.Dispatcher.Invoke((Action)(() =>
+                car.bezieraHorizontal = new Point(car.coordinates.X, car.coordinates.Y);
+                car.bezieraVertical = new Point(car.coordinates.X + 90, car.coordinates.Y);
+                while (car.bezieraVertical.Y < 290)
                 {
-                    MoveCar(xy, car1);
+                    car.coordinates = betweenPoint(car.bezieraHorizontal, car.bezieraVertical, car.procentTurn, 90);
+                    MoveCar(car.coordinates, image, car.skret);
+                    car.bezieraHorizontal.X += car.skret;
+                    car.bezieraVertical.Y++;
+                    car.procentTurn += car.skret;
+                    if (car.bezieraVertical.Y == 200)
+                    {
+                        car.skret = car.skret * -1;
+                        car.bezieraHorizontal.Y = 290;
+                    }
+                    System.Threading.Thread.Sleep(car.speed * 2);
+                }
+                while (car.coordinates.X > 170)
+                {
+                    MoveCar(car.coordinates, image, car.skret);
+                    System.Threading.Thread.Sleep(car.speed);
+                    car.coordinates.X--;
+                }
+                car.bezieraHorizontal.X = 170;
+                car.bezieraVertical.X = 100;
+                while (car.bezieraVertical.Y < 410)
+                {
+                    car.coordinates = betweenPoint(car.bezieraHorizontal, car.bezieraVertical, car.procentTurn, 60);
+                    MoveCar(car.coordinates, image, car.skret);
+                    car.bezieraHorizontal.X += car.skret;
+                    car.bezieraVertical.Y++;
+                    car.procentTurn += car.skret * -1;
+                    if (car.bezieraVertical.Y == 350)
+                    {
+                        car.skret = car.skret * -1;
+                        car.bezieraHorizontal.Y = 410;
+                    }
+                    System.Threading.Thread.Sleep(car.speed * 2);
+                }
+                while (car.coordinates.X < 800)
+                {
+                    while (lightSignal && car.coordinates.X > 590 && car.coordinates.X < 610)
+                    {
+                        System.Threading.Thread.Sleep(50);
+                    }
+                    MoveCar(car.coordinates, image, car.skret);
+                    System.Threading.Thread.Sleep(car.speed);
+                    car.coordinates.X++;
+                }
+                car.getNewValues(rand);
+                Dispatcher.Invoke((Action)(() =>
+                {
+                    Canvas.SetLeft(image, car.coordinates.X);
+                    Canvas.SetTop(image, car.coordinates.Y);
                 }));
-                System.Threading.Thread.Sleep(5);
-                xy.X++;
             }
         }
 
@@ -189,18 +164,74 @@ namespace Projekt_Systemy
             Thread tThread = new Thread(new ThreadStart(StartTrain));
             tThread.IsBackground = true;
             tThread.Start();
-            Thread cThread = new Thread(new ThreadStart(StartCar));
-            cThread.IsBackground = true;
-            cThread.Start();
+
             Thread lThread = new Thread(new ThreadStart(StartLights));
             lThread.IsBackground = true;
             lThread.Start();
+
+            Car car1 = new Car();
+            Thread c1Thread = new Thread(()=> StartCar(car1, carImg1));
+            c1Thread.IsBackground = true;
+            c1Thread.Start();
+
+            Car car2 = new Car();
+            Thread c2Thread = new Thread(() => StartCar(car2, carImg2));
+            c2Thread.IsBackground = true;
+            c2Thread.Start();
+
+            Car car3 = new Car();
+            Thread c3Thread = new Thread(() => StartCar(car3, carImg3));
+            c3Thread.IsBackground = true;
+            c3Thread.Start();
+
+            Car car4 = new Car();
+            Thread c4Thread = new Thread(() => StartCar(car4, carImg4));
+            c4Thread.IsBackground = true;
+            c4Thread.Start();
+
+            Car car5 = new Car();
+            Thread c5Thread = new Thread(() => StartCar(car5, carImg5));
+            c5Thread.IsBackground = true;
+            c5Thread.Start();
+
+            Car car6 = new Car();
+            Thread c6Thread = new Thread(() => StartCar(car6, carImg6));
+            c6Thread.IsBackground = true;
+            c6Thread.Start();
         }
 
-        private void MoveCar(Point xy, Image car)
+        private void MoveCar(Point xy, Image car, int collisionright)
         {
-            Canvas.SetLeft(car, xy.X);
-            Canvas.SetTop(car, xy.Y);
+            bool collision = true;
+            while (collision)
+            {
+                Dispatcher.Invoke((Action)(() =>
+                {
+                    foreach (FrameworkElement sibling in canvas.Children)
+                    {
+                        if (sibling.Name != car.Name)
+                        {
+                            double distance = Point.Subtract(new Point(xy.X + 25, xy.Y + 25), new Point(Convert.ToInt32(sibling.GetValue(Canvas.LeftProperty))+25, Convert.ToInt32(sibling.GetValue(Canvas.TopProperty))+25)).Length;
+
+                            if (Convert.ToInt32(distance)<75 &&
+                            Convert.ToInt32(sibling.GetValue(Canvas.TopProperty))>=xy.Y &&
+                            ((Convert.ToInt32(sibling.GetValue(Canvas.LeftProperty))>(xy.X)&&collisionright==1) || (Convert.ToInt32(sibling.GetValue(Canvas.LeftProperty)) < (xy.X + 50) && collisionright == -1))
+                            )
+                            {
+                                collision = true;
+                                break;
+                            }
+                        }
+                        collision = false;
+                    }
+                }));
+                Thread.Sleep(1);
+            }
+            Dispatcher.Invoke((Action)(() =>
+            {
+                Canvas.SetLeft(car, xy.X);
+                Canvas.SetTop(car, xy.Y);
+            }));
         }
     }
 }
